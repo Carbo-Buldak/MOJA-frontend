bbbc8c4ecd92c18d4d7037e88f238d997e231db5
gimtommang11 <gimtommang88@gmail.com>
2020-07-09 09:10:51 +0900

    create Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './style';
+
+const Footer = () => {
+  return <S.FooterWrapper />;
+};
+
+export default Footer;
---
+++ src/components/Footer/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  position: absolute;
+  height: 12.5rem;
+  background-color: #fff1c7;
+  bottom: 0;
+`;
