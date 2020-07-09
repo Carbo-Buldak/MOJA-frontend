fb1ae2a9b71d4aa619fd1342636cc2900fb4ed14
gimtommang11 <gimtommang88@gmail.com>
2020-07-09 22:30:33 +0900

    Create VideoList

---
+++ src/components/VideoList/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import * as S from './style';
+import VideoListItem from './VideoListItem/index';
+
+const VideoList = ({ videoListData }) => {
+  return (
+    <S.VideoListWrapper>
+      {videoListData != null ? (
+        videoListData.videos.map((video) => (
+          <VideoListItem
+            url={video.url}
+            title={video.title}
+            count={video.count}
+          />
+        ))
+      ) : (
+        <div>영상이 없어요</div>
+      )}
+    </S.VideoListWrapper>
+  );
+};
+
+export default VideoList;
