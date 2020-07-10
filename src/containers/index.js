b167fb10dbd68ec6da2087864ecfdcca81c2645a
gimtommang11 <gimtommang88@gmail.com>
2020-07-10 10:16:19 +0900

    import components in index.js

---
+++ src/containers/index.js
@@ -0,0 +1,11 @@
+import HeaderContainer from './HeaderContainer';
+import MainContainer from './MainContainer';
+import PopularVideoContainer from './PopularVideoContainer';
+import WaitingVideoContainer from './WaitingVideoContainer'
+
+export {
+  MainContainer,
+  HeaderContainer,
+  PopularVideoContainer,
+  WaitingVideoContainer
+}
