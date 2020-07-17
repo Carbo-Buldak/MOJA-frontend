27e2b66dc7b3e69dc5e528d49461530000b71198
gimtommang11 <gimtommang88@gmail.com>
2020-07-17 17:44:24 +0900

    create redux store

---
+++ src/modules/index.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { all, call } from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
+
+import apply from './apply/apply';
+
+const rootReducer = combineReducers({
+  apply,
+});
+
+const sagaMiddleWare = createSagaMiddleware();
+
+function* rootSaga() {
+  yield all([]);
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleWare)),
+);
+
+export default store;
+
+sagaMiddleWare.run(rootSaga);
