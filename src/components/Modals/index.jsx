7e2e1f72e69ee4a5d37fea975c5b544397f042ce
gimtommang11 <gimtommang88@gmail.com>
2020-07-17 23:10:20 +0900

    add switchModal function in modal componet

--- src/components/Modals/index.jsx
+++ src/components/Modals/index.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 import * as S from './style';
 import Apply from '../Apply';
 
-const Modal = () => {
-  return (
-    <>
-      <Apply />
-      <S.ModalOverlay />
-    </>
-  );
+const Modal = (modalType) => {
+  const switchModal = (modalType) => {
+    switch (modalType.modalType) {
+      case 'apply':
+        return (
+          <>
+            <Apply />
+            <S.ModalOverlay />
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+  return <>{switchModal(modalType)}</>;
 };
 
 export default Modal;
