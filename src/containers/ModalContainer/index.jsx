ab9b9084c67d09ba06f8b5b1bb318cb733b89f4d
gimtommang11 <gimtommang88@gmail.com>
2020-07-17 17:38:09 +0900

    create modalContainer

---
+++ src/containers/ModalContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Modal } from '../../components';
+const ModalContainer = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
+
+export default ModalContainer;
