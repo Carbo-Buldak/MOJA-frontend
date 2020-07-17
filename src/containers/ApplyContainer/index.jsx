0b727d1a9ab04f403407582e476cd7e0ed7e2ddf
gimtommang11 <gimtommang88@gmail.com>
2020-07-17 17:57:24 +0900

    create ApplyContainer

---
+++ src/containers/ApplyContainer/index.jsx
@@ -0,0 +1,22 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { getApplyData } from '../../modules/apply/apply';
+
+import { ApplyModal } from '../../components';
+
+const ApplyContainer = () => {
+  const dispatch = useDispatch();
+
+  const setApplyData = useCallback((title, url) => {
+    console.log(title, url);
+    dispatch(getApplyData(title, url));
+  }, []);
+
+  return (
+    <div>
+      <ApplyModal getApplyData={setApplyData} />
+    </div>
+  );
+}; 
+
+export default ApplyContainer;
