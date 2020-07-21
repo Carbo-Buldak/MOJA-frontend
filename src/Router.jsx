974d281e3fd6c69239bfe598e67a790fa89690ea
gimtommang11 <gimtommang88@gmail.com>
2020-07-21 21:39:17 +0900

    add popularVideo, WaitingVideo router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import { MainContainer } from './containers';
+import {
+  MainContainer,
+  PopularVideoContainer,
+  WaitingVideoContainer,
+} from './containers';
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={MainContainer} />
+    <Route path="/popularVideo" component={PopularVideoContainer} />
+    <Route path="/waitingVideo" component={WaitingVideoContainer} />
     <Route component={() => <Redirect to="/" />} />
   </Switch>
 );
