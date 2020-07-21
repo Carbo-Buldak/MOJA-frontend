8da21a5dc288d3dab88b68dde1c15018c295e9ea
gimtommang11 <gimtommang88@gmail.com>
2020-07-21 21:02:25 +0900

    add VideoList link other compoent

--- src/components/Main/MainVideoList/index.jsx
+++ src/components/Main/MainVideoList/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import * as S from './style';
 import * as Icon from '../../../assets';
@@ -9,17 +10,27 @@ const MainVideoList = ({ videoType, videoListData }) => {
   return (
     <S.MainVideoListWrapper>
       {videoType === 0 ? (
-        <VideoType
-          videoType="자막을 기다리는 영상"
-          videoTypeIcon={Icon.feed.wait_video}
-        />
+        <>
+          <VideoType
+            videoType="자막을 기다리는 영상"
+            videoTypeIcon={Icon.feed.wait_video}
+          />
+          <Link to="/waitingVideo">
+            <S.ShowMoreVideosBtn>더보기</S.ShowMoreVideosBtn>
+          </Link>
+        </>
       ) : (
-        <VideoType
-          videoType="인기 영상"
-          videoTypeIcon={Icon.feed.popular_video}
-        />
+        <>
+          <VideoType
+            videoType="인기 영상"
+            videoTypeIcon={Icon.feed.popular_video}
+          />
+          <Link to="/popularVideo">
+            <S.ShowMoreVideosBtn>더보기</S.ShowMoreVideosBtn>
+          </Link>
+        </>
       )}
-      <S.ShowMoreVideosBtn>더보기</S.ShowMoreVideosBtn>
+
       <VideoList videoListData={videoListData} />
     </S.MainVideoListWrapper>
   );
